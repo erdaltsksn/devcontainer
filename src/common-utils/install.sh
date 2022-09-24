d62eb376dfe9706b2165adf6bf1e34f14a762f27
Erdal TAŞKESEN <erdal@taskesen.me>
2022-09-25 00:10:40 +0300

    redo: Put `apt options` after the sub-command

--- src/common-utils/install.sh
+++ src/common-utils/install.sh
@@ -8,24 +8,24 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 # Fetch the latest version of the package list from Ubuntu's repositories.
-apt -y update
+apt update -y
 
 # Get to latest versions of all packages.
-apt -y upgrade --no-install-recommends
+apt upgrade -y --no-install-recommends
 
 # Install must-have applications.
-apt -y install --no-install-recommends \
+apt install -y --no-install-recommends \
     ca-certificates \
     curl \
     wget \
     tzdata \
     software-properties-common \
 # Install common applications.
-apt -y install --no-install-recommends \
+apt install -y --no-install-recommends \
     openssh-client \
     gnupg2 \
 # Install useful applications.
-apt -y install --no-install-recommends \
+apt install -y --no-install-recommends \
     git \
     make \
     unzip \
