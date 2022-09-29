6c6028ffbe44f74aa2ce55c7cad223ff5164c5a6
Erdal TAŞKESEN <erdal@taskesen.me>
2022-09-29 22:43:20 +0000

    feat: Add new devcontainer feature 'hugo'

---
+++ src/hugo/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Ensure apt is in non-interactive to avoid prompts
+# https://bugs.debian.org/929417
+export DEBIAN_FRONTEND=noninteractive
+
+# Fetch the latest version of the package list from Ubuntu's repositories.
+apt update -y
+
+# Install requirements.
+apt install -y --no-install-recommends \
+    ca-certificates \
+    curl \
+    wget \
+    tar \
+
+# Clean up.
+apt autoremove -y && \
+rm -rf /var/lib/apt/lists/*
+
+# Fetch latest version of Hugo if needed.
+if [ "${VERSION}" = "latest" ]; then
+    export VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4)}')
+fi
+
+# Build hugo package file name.
+HUGO_FILE="${VARIANT}_${VERSION}_Linux-64bit.tar.gz"
+
+# Download binaries from the release page.
+wget "https://github.com/gohugoio/hugo/releases/download/v${VERSION}/${HUGO_FILE}"
+
+# Prepare folders.
+mkdir -p /usr/local/bin
+# Unpack downloaded content.
+tar -zxf "${HUGO_FILE}" -C /usr/local/bin
+
+# Clean the leftovers.
+rm "${HUGO_FILE}"
---
+++ test/hugo/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This test file will be executed against an auto-generated devcontainer.json
+# that includes the 'hugo' feature with no options.
+#
+# Eg:
+# {
+#    "image": "<..some-base-image...>",
+#    "features": {
+#      "hugo": {}
+#    }
+# }
+#
+# Thus, the value of all options, will fall back to the default value in the 
+# feature's 'devcontainer-feature.json'
+# 
+# This test can be run with the following command (from the root of this repo)
+#   devcontainer features test . --features hugo --base-image ubuntu:22.04
+
+set -e
+
+# Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "hugo" hugo version | grep "+extended"
+
+# Report result
+reportResults
